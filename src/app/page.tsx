46836699d71dd55565c3d16c8cff248b3b5edf1c
alainmlim-dev <148725305+alainmlim-dev@users.noreply.github.com>
2025-05-20 16:56:50 +0800

    dasdasd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
-"Use client"
+"use client";
+
+import { FormEvent, useState } from "react";
 
 const centerVh = {
-  // display: "flex",
+  display: "flex",
   margin: "auto",
   justifyContent: "center",
-  alignItems: "center"
+  alignItems: "center",
+  border: "1px solid cyan",
+  padding: "32px"
 }
 
 export default function Home() {
+
+  const [inputVal, setInputVal] = useState("")
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Clicked")
+  }
+
   return (
-    <div style={centerVh}>
-      <h1 style={{fontSize: "5rem"}}>NextJS Project</h1>
-      <form style={{display: "flex", flexDirection: "column", maxWidth: "600px"}}>
-        <input type="text"></input>
-        <button>Submit</button>
-      </form>
-    </div>
+    <>
+      <h1 style={{ fontSize: "5rem", textAlign: "center" }}>NextJS Project</h1>
+      <div style={centerVh}>
+        <form style={{ display: "flex", flexDirection: "column", maxWidth: "600px" }} onSubmit={handleSubmit}>
+          <input type="text" placeholder="Type name here..." style={{ border: "1px solid red" }}></input>
+          <button style={{marginTop: "16px", cursor: "pointer"}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Predict Data
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
