4a7182a9d673ac0bc549a15568972fbe43d6d5dc
alainmlim-dev <148725305+alainmlim-dev@users.noreply.github.com>
2025-05-25 18:06:56 +0800

    052525.0606p

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,19 +1,30 @@
+import { Typography } from "@mui/material";
+import { Suspense } from "react";
+
 export default async function Page() {
 
-    const data = await fetch('http://localhost:3000/api/users')
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
     const users = await data.json()
 
     return (
-        <>
-            {users ?
-                <ul>
-                    {users.map((u: any) => (
-                        <li key={u.id}>{u.name}</li>
-                    ))}
-                </ul>
-                :
-                <p>Loading...</p>
-            }
-        </>
+        <div>
+            <Typography variant="h2" sx={{ mb: 2 }}>
+                User List
+            </Typography>
+
+            <Suspense>
+                {users.length > 0 ?
+                    <ul>
+                        {users.map((u: any) => (
+                            <li key={u.id}>{u.name}</li>
+                        ))}
+                    </ul>
+                    :
+                    <p>Loading...</p>
+                }
+            </Suspense>
+        </div>
+
+
     )
 }
